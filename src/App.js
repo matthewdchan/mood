d31bcbadfdcf875ba504522b6e7fd5f7226d39a1
Laura Lutz <greyllama@yahoo.com>
2024-02-10 17:35:45 -0500

    added journalContext

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import Form from './components/Journalform/Form';
+import Header from './components/PageLayout/Header'
+import { useJournalContext } from './JournalContext';
 
 function App() {
   return (
     <div className="App">
-      <Form/>
+    <JournalProvider>
+        {children}
+        <Form/>
+    </JournalProvider>
+
     </div>
   );
 }
---
+++ src/JournalContext.js
@@ -0,0 +1,31 @@
+import { useContext } from "react";
+import { createContext, useEffect } from "react";
+import { useState } from "react";
+import axios from 'axios';
+
+const JournalContext = createContext(); 
+
+export const JournalProvider = ( {children} ) => {
+    const [journalBlocks, setjournalBlocks] = useState([]);
+
+   useEffect(() => {
+    console.log('Context Journal:', journalBlocks);
+    axios.get('http://localhost:4000/journals')
+    .then((response) => {
+        setjournalBlocks(response.data);
+    })
+    .catch(error => {
+        console.log(error);
+    });
+   }
+   , []);
+    
+   return (
+    <JournalContext.Provider value={{journalBlocks, setjournalBlocks}}>
+        {children}
+    </JournalContext.Provider>
+   );
+};
+    export const useJournalContext = () => {
+        return useContext(JournalContext);
+    };
