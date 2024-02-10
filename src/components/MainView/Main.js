e6c5c6563832a65de5dcaf71fd70640ec33e0fed
Matthew <matthewdchan01@gmail.com>
2024-02-10 18:59:47 -0500

    added initial route

--- src/components/MainView/Main.js
+++ src/components/MainView/Main.js
@@ -1,8 +1,16 @@
 import React from "react";
+import  Header  from "../PageLayout/Header";
+import  Form from "../Journalform/Form";
+import  Footer  from "../PageLayout/Footer";
+import Maincontent from "../PageLayout/Maincontent";
 
 const Main = (props) => {
     return (
-        <p> {props.children} </p>
+        <>
+            <Header> </Header>
+            <Maincontent> </Maincontent>
+            <Footer> </Footer>
+        </>
     )
 };
 
