82ddf852a5d255de3fb9826eef93abfa5854010f
Laura Lutz <greyllama@yahoo.com>
2024-02-11 00:37:40 -0500

    changed app name

--- src/components/PageLayout/Header.js
+++ src/components/PageLayout/Header.js
@@ -9,7 +9,7 @@ import React from 'react';
 function Header(props){
     return(
         <header className='header'>
-            <h2>mood</h2>
+            <h2>moo.d</h2>
             <p>“To anyone out there who’s hurting — it’s not a sign of weakness to ask for help. It’s a sign of strength.”</p>
             <p> - Barack Obama </p>
             <p>National Suicide Prevention Lifeline: 988 or 1-800-273-8255 (TALK)</p>
