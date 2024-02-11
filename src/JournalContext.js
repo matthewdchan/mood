a3bc65d500bccb570ea4a0b89b124a196da52867
Matthew <matthewdchan01@gmail.com>
2024-02-10 19:05:45 -0500

    edited journalprovider

--- src/JournalContext.js
+++ src/JournalContext.js
@@ -1,32 +1,38 @@
-import { useContext } from "react";
 import { createContext, useEffect } from "react";
+import { useContext } from "react";
 import { useState } from "react";
 import axios from 'axios';
 
-const JournalContext = createContext(); 
+/*
+ A global state variable for the journal array
+ used throughout every view in the app to ensure consistency
+ and proper updates on change
+*/
 
+const JournalContext = createContext();
 
-export const JournalProvider = ( {children} ) => {
-    const [journalBlocks, setjournalBlocks] = useState([]);
+export const JournalProvider = ( {children}) => {
+    const [journalblocks, setjournalblocks] = useState([]);
 
-   useEffect(() => {
-    console.log('Context Journal:', journalBlocks);
-    axios.get('http://localhost:4000/journals')
-    .then((response) => {
-        setjournalBlocks(response.data);
-    })
-    .catch(error => {
-        console.log(error);
-    });
-   }
-   , []);
-    
-   return (
-    <JournalContext.Provider value={{journalBlocks, setjournalBlocks}}>
-        {children}
-    </JournalContext.Provider>
-   );
+    useEffect(() => {
+        console.log('Context Journalblocks: ', journalblocks);
+        axios.get('http://localhost:4000/journals')
+        .then((response) => {
+          setjournalblocks(response.data);
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+    , []);
+
+    return (
+        <JournalContext.Provider value={{journalblocks, setjournalblocks}}>
+            {children}
+        </JournalContext.Provider>
+    );
 };
-    export const useJournalContext = () => {
-        return useContext(JournalContext);
-    };
+
+export const useJournalContext = () => {
+    return useContext(JournalContext);
+};
